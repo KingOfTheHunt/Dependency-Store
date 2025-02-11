903827bb78215800c5448b5bf4bccc10ff97ecdb
Davi Francisco <davifrancisco214@outlook.com>
2025-02-11 07:23:52 -0300

    Organizando a classe Program

---
+++ DependencyStore/Extensions/BuilderExtension.cs
@@ -0,0 +1,24 @@
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
+namespace DependencyStore.Extensions;
+
+public static class BuilderExtension
+{
+    public static void AddRepositories(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<SqlConnection>(x => new SqlConnection("ConnectionString"));
+        builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+        builder.Services.AddTransient<IProductRepository, ProductRepository>();
+        builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+    }
+
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHttpClient<DeliveryFeeService>();
+        builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+    }
+}
--- DependencyStore/Program.cs
+++ DependencyStore/Program.cs
@@ -1,18 +1,10 @@
-using DependencyStore.Repositories;
-using DependencyStore.Repositories.Contracts;
-using DependencyStore.Services;
-using DependencyStore.Services.Contracts;
-using Microsoft.Data.SqlClient;
+using DependencyStore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<SqlConnection>(x => new SqlConnection("ConnectionString"));
-builder.Services.AddHttpClient();
-builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
-builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+builder.AddRepositories();
+builder.AddServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
